3e5232c65945f240a72e500da564b61a2391013c
yagijin <jinjin.0327.jinjin@gmail.com>
2019-11-03 10:50:46 +0900

    FIX：修正

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const pcap = require('pcap');
 const MACADDR = require('./macAddr.js');
 const Database = require("nedb");
 require('date-utils');
-const obnizcode = reqire('./obnizcode.js'); 
+const obnizcode = require('./obnizcode.js'); 
 
 const db = new Database({ filename: "datedata.db" });
 
@@ -12,10 +12,10 @@ const app = express();
 const obniz = new Obniz(obnizcode);
 
 //Obnizのコントロール
-let flagHave = false;//ものを持っているかいないか
+let flagHave = true;//ものを持っているかいないか
 let flagExist = true;//家にいるかいないか
 let flagExistBefore = true;
-let flagAleat = false;//アラート中かどうか
+let flagAleat = true;//アラート中かどうか
 let num = 0;
 
 function recordtodatabase() {
@@ -23,6 +23,8 @@ function recordtodatabase() {
     let month = new Date().toFormat("MM");
     let day = new Date().toFormat("DD");
 
+    console.log('recordtodb');
+    
     db.loadDatabase((error) => {
         if (error !== null) {
             console.error(error);
@@ -35,18 +37,20 @@ function recordtodatabase() {
         if(index === -1){//dayが無かった場合追加
             const query = { key: 1 };
             const update = {
-                $push: { alertDays : parseInt(day) }
+                $push: { ["alertDays."+ month] : parseInt(day) }
             };
             const options = {};
             db.update(query, update, options, (error, numOfDocs) => {
             });
         }
     });
+    
 }
 
 obniz.onconnect = async function () {
     var gp2y0a21yk0f;
     gp2y0a21yk0f = obniz.wired("GP2Y0A21YK0F",{"vcc":0, "gnd":1, "signal":2});
+    const speaker = obniz.wired("Speaker", {signal:3, gnd:4});
     await delayms(500);
     //flagExistは外部から制御
     while (true) {
@@ -67,21 +71,27 @@ obniz.onconnect = async function () {
             //何もしない
         }else if(!flagHave&&flagExist){//持って無くている
             if(num > 10){//持ち出したらflagHaveを変える
-                flagHave = false;
+                flagHave = true;
+                flagExist = false;
+                console.log("item has gone");
             }
         }else if(!flagHave&&!flagExist){//持って無くていない
             //何もしない
         }
 
         if(flagAleat){
-            //アラートを鳴らす処理を書く
+            //アラートを鳴らす処理を書く  
+            await delayms(0);      
+            speaker.play(1000); 
+            await obniz.wait(100);
+            speaker.stop();
             console.log("Alerting");
         }
 
         await delayms(0);
         console.log((await gp2y0a21yk0f.getWait()));
         console.log(num);
-        if (await flagHave ? (gp2y0a21yk0f.getWait() <= 300) : (gp2y0a21yk0f.getWait() > 300)) {
+        if ((flagHave ? (await gp2y0a21yk0f.getWait() <= 300) : (await gp2y0a21yk0f.getWait() > 300))) {
             num = (typeof num == 'number' ? num : 0) + 1;
         }
         flagExistBefore = flagExist;
@@ -106,7 +116,7 @@ app.get('/api/v1/getdata', (req, res) => {
     });
     
     db.find({ "key" : 1 }, (error, docs) => {
-        res.json(docs[0].alertDays);
+        res.json(docs[0].alertDays.sort());
         //console.log(docs[0].alertDays["10"]);
     });
 });
@@ -130,7 +140,6 @@ let pcapSession = pcap.createSession(NET_INTERFACE, "arp")
 
 //ターゲットが帰って来たら
 function targetReturn () {
-    
 }
 
 pcapSession.on('packet', function (raw_packet) {
